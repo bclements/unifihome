1abc7738e0cccea8bbdafe92a0abc3f365b84735
Brent Clements <brent.clements@gmail.com>
2022-03-25 14:39:22 -0500

    Added more changes

--- setup.py
+++ setup.py
@@ -1,32 +1,27 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(
-    name="unifihome",
-    version="0.1.0",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    install_requires=[
-        "black==22.1.0",
-        "certifi==2021.10.8",
-        "charset-normalizer==2.0.12",
-        "click==8.0.4",
-        "commonmark==0.9.1",
-        "idna==3.3",
-        "mypy-extensions==0.4.3",
-        "pathspec==0.9.0",
-        "platformdirs==2.5.1",
-        "Pygments==2.11.2",
-        "pyunifi==2.21",
-        "requests==2.27.1",
-        "rich==12.0.0",
-        "textual==0.1.17",
-        "textual-inputs==0.2.5",
-        "tomli==2.0.1",
-        "typing-extensions==4.1.1",
-        "urllib3==1.26.9",
-    ],
-    packages=[
-        "unifihome",
-        "unifihome.ui",
-    ],
-)
+packages = \
+['unifihome', 'unifihome.model', 'unifihome.ui', 'unifihome.ui.systeminfo']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'unifihome',
+    'version': '0.1.1',
+    'description': 'Console based application for monitoring and management of at home UNIFI devices',
+    'long_description': None,
+    'author': 'Brent Clements',
+    'author_email': 'brent.clements@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
+
--- unifihome/ui/systeminfo/page.py
+++ unifihome/ui/systeminfo/page.py
@@ -6,29 +6,29 @@ from textual._context import active_app
 from textual.widget import Widget
 
 #from ...model.systeminfo import SystemInfo
-from ..systeminfo.backup import BackupInfoWidget
-from ..systeminfo.build import BuildInfoWidget
-from ..systeminfo.misc import MiscInfoWidget
-from ..systeminfo.network import NetworkInfoWidget
+#from ..systeminfo.backup import BackupInfoWidget
+#from ..systeminfo.build import BuildInfoWidget
+#from ..systeminfo.misc import MiscInfoWidget
+#from ..systeminfo.network import NetworkInfoWidget
 
 
-class PageWidget(Widget):
-    def render(self) -> RenderableType:
-        systeminfo = SystemInfo.from_api(
-            active_app.get().unifi_controller.get_sysinfo()
-        )
-        networkWidget = NetworkInfoWidget(data=systeminfo)
-        buildWidget = BuildInfoWidget(data=systeminfo)
-        backupWidget = BackupInfoWidget(data=systeminfo)
-        miscWidget = MiscInfoWidget(data=systeminfo)
+#class PageWidget(Widget):
+#    def render(self) -> RenderableType:
+#        systeminfo = SystemInfo.from_api(
+#            active_app.get().unifi_controller.get_sysinfo()
+#        )
+#        networkWidget = NetworkInfoWidget(data=systeminfo)
+#        buildWidget = BuildInfoWidget(data=systeminfo)
+#        backupWidget = BackupInfoWidget(data=systeminfo)
+#        miscWidget = MiscInfoWidget(data=systeminfo)
 
-        self.table = Table(expand=True, show_header=False, padding=0, box=None)
-        self.table.add_column(no_wrap=True, ratio=1)
-        self.table.add_column(no_wrap=True)
-        self.table.add_column(no_wrap=True)
-        self.table.add_row(buildWidget, backupWidget, "")
-        self.table.add_row(networkWidget, miscWidget, "")
-        self.group = Group(self.table, "")
-        return Padding(
-            Panel(self.group, title="Unifi System Information"), (1, 0, 0, 0)
-        )
+#        self.table = Table(expand=True, show_header=False, padding=0, box=None)
+#        self.table.add_column(no_wrap=True, ratio=1)
+#        self.table.add_column(no_wrap=True)
+#        self.table.add_column(no_wrap=True)
+#        self.table.add_row(buildWidget, backupWidget, "")
+#        self.table.add_row(networkWidget, miscWidget, "")
+#        self.group = Group(self.table, "")
+#        return Padding(
+#            Panel(self.group, title="Unifi System Information"), (1, 0, 0, 0)
+#        )
